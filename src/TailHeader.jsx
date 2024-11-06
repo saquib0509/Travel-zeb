ef393a9cecd9714b967528fa517ec0581b8b9682
Saquib <saquibakhtar0509@gmail.com>
2024-11-06 21:04:55 +0530

    resposive changes

--- src/TailHeader.jsx
+++ src/TailHeader.jsx
@@ -170,7 +170,7 @@ export default function TailHeader() {
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
                                 >
-                                    Marketplace
+                                    Products
                                 </a>
                                 <a
                                     href="#"
