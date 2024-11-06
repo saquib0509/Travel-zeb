23e508d177d143c3cd4509ccce0d442fbc021cfe
Saquib <saquibakhtar0509@gmail.com>
2024-11-06 21:01:09 +0530

    changes in navbar

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
         <Cards />
         <Footer/>
       </div>
-
     </>
-
   );
 }
 
--- src/TailHeader.jsx
+++ src/TailHeader.jsx
@@ -40,7 +40,7 @@ export default function TailHeader() {
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
+                        <span className="sr-only">Travel zeb</span>
                         <img
                             alt=""
                             src={beach}
@@ -106,7 +106,7 @@ export default function TailHeader() {
                         Features
                     </a>
                     <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                        Marketplace
+                        Products
                     </a>
                     <a href="#" className="text-sm/6 font-semibold text-gray-900">
                         Company
